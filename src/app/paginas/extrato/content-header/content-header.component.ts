871dc921109e2bed1f51ab079f75551a5afff8bc
Atila Sobrinho <atilagsobrinho@gmail.com>
2024-02-09 17:16:44 -0300

    feature108: Criação de header-extrato component e content-extrato component.

---
+++ src/app/paginas/extrato/content-header/content-header.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-content-header',
+  standalone: true,
+  imports: [],
+  templateUrl: './content-header.component.html',
+  styleUrl: './content-header.component.css'
+})
+export class ContentHeaderComponent {
+
+}
