5e2e4273b42832664e8a4079e0d43601edfa7cdd
Vanessa Molina <vanessakrmolina@hotmail.com>
2024-02-09 14:42:16 -0300

    feature101: configuração de rotas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { ExtratoComponent } from './paginas/extrato/extrato.component';
+import { HomeComponent } from './paginas/home/home.component';
+import { LoginComponent } from './paginas/login/login.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'login', component: LoginComponent },
+    { path: 'home', component: HomeComponent },
+    { path: 'extrato', component: ExtratoComponent },
+];
+
+@NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
+})
+
+export class AppRoutingModule { }
