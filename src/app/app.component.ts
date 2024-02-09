6bdc313c42464d7737bf46e280c7c42186867ce5
Amanda Santos <amanda280721@gmail.com>
2024-02-09 16:58:50 -0400

    feature103:Iniciando a tela de login

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,5 +11,5 @@ import { ExtratoComponent } from './paginas/extrato/extrato.component';
   styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'app-pay';
+  title = 'Tela de Login';
 }
