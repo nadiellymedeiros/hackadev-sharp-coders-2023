76e8d139b20e8cac92de2a4fba26442b30c245d3
nadiellymedeiros <nadiellymedeiros@gmail.com>
2024-02-08 22:02:47 -0300

    delete foulder historico and create folder extrato

---
+++ src/app/paginas/extrato/extrato.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-extrato',
+  standalone: true,
+  imports: [],
+  templateUrl: './extrato.component.html',
+  styleUrl: './extrato.component.css'
+})
+export class ExtratoComponent {
+
+}
