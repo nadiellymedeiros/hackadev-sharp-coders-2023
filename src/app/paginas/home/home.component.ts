113f15b532eecb406b524c64664934e1461ab423
klaynmolina <vanessakrmolina@hotmail.com>
2024-02-12 17:18:03 -0300

    desenvolvimento página inicial (content home)

--- src/app/paginas/home/home.component.ts
+++ src/app/paginas/home/home.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ContentHomeComponent } from "./content-home/content-home.component";
 
 @Component({
-  selector: 'app-home',
-  standalone: true,
-  imports: [CommonModule],
-  templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+    selector: 'app-home',
+    standalone: true,
+    templateUrl: './home.component.html',
+    styleUrl: './home.component.css',
+    imports: [CommonModule, ContentHomeComponent]
 })
 export class HomeComponent {
 
