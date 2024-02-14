0eaa8ae0a943b64fa6990d31a5c1711deea275c0
Alan Vargas <alanvargasenf@gmail.com>
2024-02-14 11:17:22 -0300

    creation of the login desktop component

--- src/app/paginas/login/login.component.ts
+++ src/app/paginas/login/login.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MobileComponent } from './mobile/mobile.component';
+import { DesktopComponent } from "./desktop/desktop.component";
 
 @Component({
-  selector: 'app-login',
-  standalone: true,
-  imports: [CommonModule, MobileComponent],
-  templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+    selector: 'app-login',
+    standalone: true,
+    templateUrl: './login.component.html',
+    styleUrl: './login.component.css',
+    imports: [CommonModule, MobileComponent, LoginComponent, DesktopComponent]
 })
 export class LoginComponent {
 
