1a71ac79ab68ce721a7bc6b6dc2c30682f2d5feb
Amanda Santos <amanda280721@gmail.com>
2024-02-14 10:00:33 -0400

    feature103: Tela de Login mobile

--- src/app/paginas/login/login.component.ts
+++ src/app/paginas/login/login.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MobileComponent } from './mobile/mobile.component';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, MobileComponent],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
---
+++ src/app/paginas/login/mobile/mobile.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-mobile',
+  standalone: true,
+  imports: [],
+  templateUrl: './mobile.component.html',
+  styleUrl: './mobile.component.css'
+})
+export class MobileComponent {
+
+}
