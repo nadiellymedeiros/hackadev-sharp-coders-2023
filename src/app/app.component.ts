8916f6d82a20647bc5359782bf4271aa0d742af3
Vanessa Molina <vanessakrmolina@hotmail.com>
2024-02-15 18:59:27 -0300

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,14 +2,13 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { ExtratoComponent } from './paginas/extrato/extrato.component';
-import { LoginComponent } from "./paginas/login/login.component";
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [CommonModule, RouterOutlet, ExtratoComponent, LoginComponent]
+    imports: [CommonModule, RouterOutlet, ExtratoComponent]
 })
 export class AppComponent {
   title = 'app-pay';
