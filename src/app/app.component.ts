271939db4059172462a2d13686e40f15b47bae38
Vanessa Molina <vanessakrmolina@hotmail.com>
2024-02-15 18:50:13 -0300

    ajuste botões header-home

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,4 +12,18 @@ import { ExtratoComponent } from './paginas/extrato/extrato.component';
 })
 export class AppComponent {
   title = 'app-pay';
+
+  constructor(private router:Router){}
+
+    homeRouter (): void { 
+        this.router.navigateByUrl('/home');
+    }
+
+    extrato (): void { 
+        this.router.navigateByUrl('/extrato');
+    }
+
+    poupanca (): void { 
+        this.router.navigateByUrl('/poupanca');
+    }
 }
