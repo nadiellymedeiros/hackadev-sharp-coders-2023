f70307bddece0396dfd754099d8c1cf81c938b86
Vanessa Molina <vanessakrmolina@hotmail.com>
2024-02-15 18:42:15 -0300

    ajuste content-home

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+import { Router, RouterOutlet } from '@angular/router';
 import { ExtratoComponent } from './paginas/extrato/extrato.component';
 import { LoginComponent } from "./paginas/login/login.component";
 
@@ -13,4 +13,19 @@ import { LoginComponent } from "./paginas/login/login.component";
 })
 export class AppComponent {
   title = 'app-pay';
+
+  constructor(private router:Router){}
+
+    goHome (): void { 
+        this.router.navigateByUrl('/home');
+    }
+
+    goExtrato (): void { 
+        this.router.navigateByUrl('/extrato');
+    }
+
+    //Ainda não implementado
+    goPoupanca (): void { 
+        this.router.navigateByUrl('/poupanca');
+    }
 }
