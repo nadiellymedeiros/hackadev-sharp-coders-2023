1e4681756cb0a0893971817cfb51c6625f4c093f
Vanessa Molina <vanessakrmolina@hotmail.com>
2024-02-15 18:58:15 -0300

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,30 +1,15 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { Router, RouterOutlet } from '@angular/router';
+import { RouterOutlet } from '@angular/router';
 import { ExtratoComponent } from './paginas/extrato/extrato.component';
-import { LoginComponent } from "./paginas/login/login.component";
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [CommonModule, RouterOutlet, ExtratoComponent, LoginComponent]
+    imports: [CommonModule, RouterOutlet, ExtratoComponent]
 })
 export class AppComponent {
   title = 'app-pay';
-
-  constructor(private router:Router){}
-
-    homeRouter (): void { 
-        this.router.navigateByUrl('/home');
-    }
-
-    extrato (): void { 
-        this.router.navigateByUrl('/extrato');
-    }
-
-    poupanca (): void { 
-        this.router.navigateByUrl('/poupanca');
-    }
 }
