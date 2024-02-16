b0c63a1cedf20a3580de82fba921902151c73684
Atila Sobrinho <atilagsobrinho@gmail.com>
2024-02-16 20:34:49 -0300

    Feature108: Correção do modelo desktop extrato.

--- src/app/paginas/home/home.component.ts
+++ src/app/paginas/home/home.component.ts
@@ -1,19 +1,25 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ContentHomeComponent } from "./content-home/content-home.component";
-import { HeaderHomeComponent } from "./header-home/header-home.component";
-import { FooterMobileComponent } from "../../componentes/footer/footer-mobile/footer-mobile.component";
-import { FooterDesktopComponent } from "../../componentes/footer/footer-desktop/footer-desktop.component";
+import { ContentHomeComponent } from './content-home/content-home.component';
+import { HeaderHomeComponent } from './header-home/header-home.component';
+import { FooterMobileComponent } from '../../componentes/footer/footer-mobile/footer-mobile.component';
+import { FooterDesktopComponent } from '../../componentes/footer/footer-desktop/footer-desktop.component';
 
 import { Router } from '@angular/router';
+import { FooterComponent } from '../../componentes/footer/footer.component';
 
 @Component({
-    selector: 'app-home',
-    standalone: true,
-    templateUrl: './home.component.html',
-    styleUrl: './home.component.css',
-    imports: [CommonModule, ContentHomeComponent, HeaderHomeComponent, FooterComponent, FooterMobileComponent, FooterDesktopComponent]
+  selector: 'app-home',
+  standalone: true,
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.css',
+  imports: [
+    CommonModule,
+    ContentHomeComponent,
+    HeaderHomeComponent,
+    FooterComponent,
+    FooterMobileComponent,
+    FooterDesktopComponent,
+  ],
 })
-export class HomeComponent {
-
-}
+export class HomeComponent {}
