ca2c7c06f27e4f1d2f02a5aa1fa6cf781d7ecfb6
nadiellymedeiros <nadiellymedeiros@gmail.com>
2024-02-16 17:41:14 -0300

    feature107: add footer-desktop

--- src/app/paginas/extrato/extrato.component.ts
+++ src/app/paginas/extrato/extrato.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { ContentExtratoComponent } from './content-extrato/content-extrato.component';
 import { HeaderExtratoComponent } from './header-extrato/header-extrato.component';
 import { FooterMobileComponent } from '../../componentes/footer/footer-mobile/footer-mobile.component';
+import { FooterDesktopComponent } from '../../componentes/footer/footer-desktop/footer-desktop.component';
 @Component({
   selector: 'app-extrato',
   standalone: true,
@@ -9,6 +10,7 @@ import { FooterMobileComponent } from '../../componentes/footer/footer-mobile/fo
     ContentExtratoComponent,
     HeaderExtratoComponent,
     FooterMobileComponent,
+    FooterDesktopComponent,
   ],
   templateUrl: './extrato.component.html',
   styleUrl: './extrato.component.css',
