6dffcceef82d849c6cfc7cac220097373ca29bbf
nadiellymedeiros <nadiellymedeiros@gmail.com>
2024-02-16 10:42:51 -0300

    feature109: move footer to footer-mobile

---
+++ src/app/componentes/footer/footer-mobile/footer-mobile.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-footer-mobile',
+  standalone: true,
+  imports: [],
+  templateUrl: './footer-mobile.component.html',
+  styleUrl: './footer-mobile.component.css'
+})
+export class FooterMobileComponent {
+
+}
