e208f9a33e09acddedcca46c6bccbcb3dd68d7dd
nadiellymedeiros <nadiellymedeiros@gmail.com>
2024-02-16 11:10:17 -0300

    feature107: edit the footer to forrter-mobile

--- src/app/paginas/extrato/extrato.component.ts
+++ src/app/paginas/extrato/extrato.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
 import { ContentExtratoComponent } from './content-extrato/content-extrato.component';
 import { HeaderExtratoComponent } from './header-extrato/header-extrato.component';
-import { FooterComponent } from '../../componentes/footer/footer.component';
-
+import { FooterMobileComponent } from '../../componentes/footer/footer-mobile/footer-mobile.component';
 @Component({
   selector: 'app-extrato',
   standalone: true,
-  imports: [ContentExtratoComponent, HeaderExtratoComponent, FooterComponent],
+  imports: [
+    ContentExtratoComponent,
+    HeaderExtratoComponent,
+    FooterMobileComponent,
+  ],
   templateUrl: './extrato.component.html',
   styleUrl: './extrato.component.css',
 })
