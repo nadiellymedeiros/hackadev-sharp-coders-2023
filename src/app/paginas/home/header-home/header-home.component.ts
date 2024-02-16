35827e0f5ba323791cd28cdfa95453db9d174df1
Vanessa Molina <vanessakrmolina@hotmail.com>
2024-02-16 16:18:25 -0300

    feature105: ajuste rotas header-home

--- src/app/paginas/home/header-home/header-home.component.ts
+++ src/app/paginas/home/header-home/header-home.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-header-home',
   standalone: true,
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './header-home.component.html',
   styleUrl: './header-home.component.css'
 })
