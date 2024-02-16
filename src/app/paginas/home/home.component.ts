61de5c7ab6a847f7d107a8984c0e2b3e18515ebc
Vanessa Molina <vanessakrmolina@hotmail.com>
2024-02-16 18:10:59 -0300

    feature106: implementação footer no home component

--- src/app/paginas/home/home.component.ts
+++ src/app/paginas/home/home.component.ts
@@ -2,13 +2,16 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ContentHomeComponent } from "./content-home/content-home.component";
 import { HeaderHomeComponent } from "./header-home/header-home.component";
+import { FooterComponent } from "../../componentes/footer/footer.component";
+import { FooterMobileComponent } from "../../componentes/footer/footer-mobile/footer-mobile.component";
+import { FooterDesktopComponent } from "../../componentes/footer/footer-desktop/footer-desktop.component";
 
 @Component({
     selector: 'app-home',
     standalone: true,
     templateUrl: './home.component.html',
     styleUrl: './home.component.css',
-    imports: [CommonModule, ContentHomeComponent, HeaderHomeComponent]
+    imports: [CommonModule, ContentHomeComponent, HeaderHomeComponent, FooterComponent, FooterMobileComponent, FooterDesktopComponent]
 })
 export class HomeComponent {
 
