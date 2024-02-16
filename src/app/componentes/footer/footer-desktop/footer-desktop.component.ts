354661133dd65ddb2c5932207f3c12d28b93858d
nadiellymedeiros <nadiellymedeiros@gmail.com>
2024-02-16 17:36:21 -0300

    feature109: create footer-desktop

---
+++ src/app/componentes/footer/footer-desktop/footer-desktop.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-footer-desktop',
+  standalone: true,
+  imports: [],
+  templateUrl: './footer-desktop.component.html',
+  styleUrl: './footer-desktop.component.css'
+})
+export class FooterDesktopComponent {
+
+}
