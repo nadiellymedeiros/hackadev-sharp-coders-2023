72ee710a2f0a841c0cee62f912aae9a063cf6456
Alan Vargas <alanvargasenf@gmail.com>
2024-02-19 11:13:28 -0300

    Navbar-desktop update

--- src/app/componentes/navbar/navbar-desktop/navbar-desktop.component.ts
+++ src/app/componentes/navbar/navbar-desktop/navbar-desktop.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-navbar-desktop',
@@ -8,5 +9,14 @@ import { Component } from '@angular/core';
   styleUrl: './navbar-desktop.component.css'
 })
 export class NavbarDesktopComponent {
+  
+  constructor(private router: Router) {}
 
+  goToHome(): void {
+    this.router.navigateByUrl('/home');
+  }
+
+  goToExtrato(): void {
+    this.router.navigateByUrl('/extrato');
+  }
 }
