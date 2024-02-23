83a29953661fd53d4077fb3bab70879fd82f3ff5
nadiellymedeiros <nadiellymedeiros@gmail.com>
2024-02-23 10:56:23 -0300

    development: add route cartoes

---
+++ src/app/paginas/meus-cartoes/meus-cartoes.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-meus-cartoes',
+  standalone: true,
+  imports: [],
+  templateUrl: './meus-cartoes.component.html',
+  styleUrl: './meus-cartoes.component.css'
+})
+export class MeusCartoesComponent {
+
+}
