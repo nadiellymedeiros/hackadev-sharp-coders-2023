24a516d612d3b8fbd96d7b468d1bbfb47460f5bc
Douglas C Pedrosa <doug.cooimbra@gmail.com>
2024-02-23 18:55:13 -0300

    Committing changes before Angular CLI update

---
+++ src/app/componentes/meus-cartoes/meus-cartoes.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { NavbarDesktopComponent } from '../../componentes/navbar/navbar-desktop/navbar-desktop.component';
+import { NavbarMobileComponent } from '../../componentes/navbar/navbar-mobile/navbar-mobile.component';
+import { FooterDesktopComponent } from '../../componentes/footer/footer-desktop/footer-desktop.component';
+import { FooterMobileComponent } from '../../componentes/footer/footer-mobile/footer-mobile.component';
+
+@Component({
+  selector: 'app-meus-cartoes',
+  standalone: true,
+  imports: [NavbarDesktopComponent,NavbarMobileComponent,FooterDesktopComponent,FooterMobileComponent , MeusCartoesComponent],
+  templateUrl: './meus-cartoes.component.html',
+  styleUrl: './meus-cartoes.component.css'
+})
+export class MeusCartoesComponent {
+
+}
