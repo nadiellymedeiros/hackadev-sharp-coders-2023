4123520dc5d74313637e728533523952616d1f3b
Alan Vargas <alanvargasenf@gmail.com>
2024-03-01 01:37:53 -0300

    feature219: reviewing code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,13 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Router, RouterOutlet } from '@angular/router';
-import { HomePageComponent } from "./paginas/home-page/home-page.component";
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [CommonModule, RouterOutlet, HomePageComponent]
+    imports: [CommonModule, RouterOutlet]
 })
 export class AppComponent {
   title = 'app-pay'; 
