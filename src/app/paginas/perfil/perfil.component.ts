974f678f01d484153944f3a5b0587f786c108eb1
Vanessa Molina <vanessakrmolina@hotmail.com>
2024-03-01 17:50:37 -0300

    criação da página perfil

---
+++ src/app/paginas/perfil/perfil.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-perfil',
+  standalone: true,
+  imports: [],
+  templateUrl: './perfil.component.html',
+  styleUrl: './perfil.component.css'
+})
+export class PerfilComponent {
+
+}
