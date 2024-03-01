b91c4b0cd44bf92153602fea6966a0c8966a8bdc
nadiellymedeiros <nadiellymedeiros@gmail.com>
2024-03-01 20:38:44 -0300

    development: add sidebar at extrato page

--- src/app/paginas/extrato/extrato.component.ts
+++ src/app/paginas/extrato/extrato.component.ts
@@ -5,6 +5,7 @@ import { FooterMobileComponent } from '../../componentes/footer/footer-mobile/fo
 import { FooterDesktopComponent } from '../../componentes/footer/footer-desktop/footer-desktop.component';
 import { NavbarMobileComponent } from '../../componentes/navbar/navbar-mobile/navbar-mobile.component';
 import { NavbarDesktopComponent } from '../../componentes/navbar/navbar-desktop/navbar-desktop.component';
+import { SidebarComponent } from '../../componentes/sidebar/sidebar.component';
 @Component({
   selector: 'app-extrato',
   standalone: true,
@@ -15,6 +16,7 @@ import { NavbarDesktopComponent } from '../../componentes/navbar/navbar-desktop/
     FooterDesktopComponent,
     NavbarMobileComponent,
     NavbarDesktopComponent,
+    SidebarComponent,
   ],
   templateUrl: './extrato.component.html',
   styleUrl: './extrato.component.css',
