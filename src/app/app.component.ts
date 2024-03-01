3ce53a3d14616699b27b665312dc093aeea3efcc
Alan Vargas <alanvargasenf@gmail.com>
2024-03-01 01:33:06 -0300

    feature219: create home page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Router, RouterOutlet } from '@angular/router';
+import { HomePageComponent } from "./paginas/home-page/home-page.component";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css',
-  imports: [CommonModule, RouterOutlet],
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
+    imports: [CommonModule, RouterOutlet, HomePageComponent]
 })
 export class AppComponent {
   title = 'app-pay'; 
