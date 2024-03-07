5dfd3a8b52ff32875040fa5dba26bdad6d1193ff
Luan Oliveira <luanoliveiraodj@gmail.com>
2024-03-06 21:11:01 -0300

    feature115: Atualizando página de cadastro

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,7 +2,9 @@ import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { provideEnvironmentNgxMask } from 'ngx-mask';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [provideRouter(routes), provideEnvironmentNgxMask(), provideAnimationsAsync()]
 };
