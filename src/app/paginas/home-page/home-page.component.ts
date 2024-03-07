1c4f95d3fb689dd5f4d7f5c4d04a5303a78a6089
Alan Vargas <alanvargasenf@gmail.com>
2024-03-07 14:41:34 -0300

    update home-page

--- src/app/paginas/home-page/home-page.component.ts
+++ src/app/paginas/home-page/home-page.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { Router, RouterModule } from '@angular/router';
+import { FooterDesktopComponent } from "../../componentes/footer/footer-desktop/footer-desktop.component";
+import { FooterMobileComponent } from "../../componentes/footer/footer-mobile/footer-mobile.component";
 
 @Component({
     selector: 'app-home-page',
     standalone: true,
     templateUrl: './home-page.component.html',
     styleUrl: './home-page.component.css',
-    imports: [RouterModule]
+    imports: [RouterModule, FooterDesktopComponent, FooterMobileComponent]
 })
 export class HomePageComponent {
 
